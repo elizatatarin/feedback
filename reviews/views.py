714ba3ed63e8ab4f17bfb27d8c375b986d3633ed
eliza <eliza@DESKTOP-HLLIIPJ>
2023-08-27 12:28:27 +0300

    10_section

--- reviews/views.py
+++ reviews/views.py
@@ -1,24 +1,34 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
 
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
 
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
